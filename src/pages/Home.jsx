d02b3304c32512576d0237e849744eb8ff13fb84
raipratik <pratikkumar399@gmail.com>
2023-08-15 12:46:24 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../components/Navbar"
+
+const Home = () => {
+    return (
+        <div>
+            This is the Home page
+
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home
