28c0747482b33dbd779533ff47d9b4928ae15272
raipratik <pratikkumar399@gmail.com>
2023-09-23 18:58:26 +0530

    4/5th part done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,14 +27,14 @@ const Hero = () => {
                         </div>
                     </div>
                     <div className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
-                        <span className="mb-8 text-xs font-bold tracking-widest text-blue-600 dark:text-white uppercase">
+                        <span className="mb-8 text-xs font-bold tracking-widest text-blue-600 dark:text-black uppercase">
                             {" "}
                             I am{" "}
                         </span>
-                        <h1 className="fontC  mb-8 text-4xl font-bold leading-none dark:text-white tracking-tighter md:text-7xl lg:text-5xl">
+                        <h1 className="fontC  mb-8 text-4xl font-bold leading-none dark:text-black tracking-tighter md:text-7xl lg:text-5xl">
                             Pratik Rai
                         </h1>
-                        <p className="mb-8 text-xl leading-relaxed text-left dark:text-white  sfont ">
+                        <p className="mb-8 text-xl leading-relaxed text-left dark:text-black  sfont ">
                             <TypeAnimation
                                 sequence={[
                                     "I'm a Mern Stack Developer",
@@ -49,7 +49,7 @@ const Hero = () => {
                                 style={{ fontSize: '1.5em' }}
                             />
                         </p>
-                        <div className="flex dark:text-white  flex-wrap w-full mt-2 -mx-4 text-left">
+                        <div className="flex dark:text-black  flex-wrap w-full mt-2 -mx-4 text-left">
                             <div className="w-1/4 p-4 mt-4 sm:w-1/4">
                                 <DiJavascript1 />
                             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -149,10 +149,10 @@ const Navbar = () => {
                             <li className="mb-1">
                                 <Link
                                     className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded"
-                                    to="/about"
+                                    to="/intro"
                                     onClick={() => handleMenuItemClick()}
                                 >
-                                    About
+                                    Intro
                                 </Link>
                             </li>
                             <li className="mb-1">
@@ -168,10 +168,10 @@ const Navbar = () => {
                             <li className="mb-1">
                                 <Link
                                     className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded"
-                                    to="/contact"
+                                    to="/about"
                                     onClick={() => handleMenuItemClick()}
                                 >
-                                    Contact
+                                    About Me
                                 </Link>
                             </li>
                         </ul>
